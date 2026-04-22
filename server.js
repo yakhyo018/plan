7fc35e1a475042c496db1acdb0a86200f91b103a
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-04-22 13:18:16 +0900

    feat: develop  author web router

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ console.log("server ishga tushdi");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+const { error } = require("console");
+
+let user ;
+fs.readFile("database/user.json","utf8", (err, data) =>{
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // 1 kirish code
  app.use(express.static("public")); 
@@ -14,7 +25,6 @@ const http = require("http");
 // 3 views code
 app.set("views", "views");
 app.set("view engine", "ejs");
-
 // 4 routing code
 app.post("/create-item", (req, res) => {
     console.log("req.body");
@@ -23,6 +33,11 @@ app.post("/create-item", (req, res) => {
 app.get("/", function(req, res){
     res.render("harid.ejs");
 })
+
+app.get("/author", function(req, res){
+    res.render("author", { user: user });
+});
+
 const server = http.createServer(app);
 let PORT = 8000;
 server.listen(PORT, function(){
