1717a1eb2160b207ad5d9b2dbd1aff9a63f92e98
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-04-27 17:48:39 +0900

    feat: last lesson

---
+++ app.js
@@ -0,0 +1,64 @@
+console.log("server ishga tushdi");
+
+const express = require("express");
+const fs = require("fs");
+const { error } = require("console");
+const res = require("express/lib/response");
+const app = express();
+
+// mongodb choqirish
+const db = require("./server").db();
+
+let user ;
+fs.readFile("database/user.json","utf8", (err, data) =>{
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
+// 1 kirish code
+ app.use(express.static("public")); 
+ app.use(express.json());
+ app.use(express.urlencoded({extended: true}));
+
+// 2 session code
+
+// 3 views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+// 4 routing code
+app.post("/create-item", (req, res) => {
+     console.log(req.body);
+     const new_reja = req.body.reja;
+     db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            res.end("succesfully added");
+        }
+     });
+});
+
+app.get("/", function(req, res){
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if (err) {
+            console.log(err);
+            res.end("do something wrong");
+        } else {
+            console.log(data);
+            res.render("reja", { items: data });
+        }
+    });
+});
+
+app.get("/author", function(req, res){
+    res.render("author", { user: user });
+});
+
+module.exports = app;
+
--- server.js
+++ server.js
@@ -1,45 +1,29 @@
-console.log("server ishga tushdi");
-
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
-const { error } = require("console");
+const mongodb = require("mongodb");
 
-let user ;
-fs.readFile("database/user.json","utf8", (err, data) =>{
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data)
+let db;
+const connectionString = 
+"mmongodb+srv://yakhyo35:Antony1234@antony.3ola0v7.mongodb.net/";
+
+mongodb.connect(
+    connectionString,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err, client) => {
+        if(err) Console.log("ERROR on connection MongoDB");
+        else{
+            console.log("MongoDB connection succeed");
+            module.exports = client;
+
+          const app =require("./app");
+          const server = http.createServer(app);
+          let PORT = 8000;
+          server.listen(PORT, function(){
+            console.log(
+                `the server succesfully running on ${PORT}, http://localhost:${PORT}`);
+});  
+        }
     }
-});
-
-// 1 kirish code
- app.use(express.static("public")); 
- app.use(express.json());
- app.use(express.urlencoded({extended: true}));
-
-// 2 session code
-
-// 3 views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-// 4 routing code
-app.post("/create-item", (req, res) => {
-     
-});
-app.get("/", function(req, res){
-    res.render("reja.ejs");
-})
-
-app.get("/author", function(req, res){
-    res.render("author", { user: user });
-});
-
-const server = http.createServer(app);
-let PORT = 8000;
-server.listen(PORT, function(){
-    console.log(`the server succesfully running on ${PORT}, http://localhost:${PORT}`);
-});
-
+)
